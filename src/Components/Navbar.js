6060443ef2faaf01d95c1b4d214e610734ebe7fd
LongJourney47 <fernandezjw07@gmail.com>
2022-09-01 12:29:19 -0400

    feat: structured, styled, and added responsiveness to navbar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,8 +1,49 @@
 import React from 'react'
+import {  Link } from "react-router-dom";
+import Styles from '../Styles/Navbar.module.css'
 
-const App = () => {
+
+const Navbar = () => {
   return (
   <>
+  <header> 
+      <nav className={Styles.Navbar}>
+  <aside className={Styles.Title}> 
+  <span>
+  <p>PlaceHolder</p>
+  </span>
+  
+  </aside>
+        <aside className={Styles.Links}>
+          <ul>
+            <li>
+            <Link className={Styles.LinkStyles}  to="/" >
+             Home 
+              </Link>
+            </li>
+            
+          
+            <li>
+            <Link className={Styles.LinkStyles}  to="/About" > 
+          <span>About </span> </Link>
+            </li>
+            
+          <span>
+            <li>
+            <Link className={Styles.LinkStyles}  to="/Skills" >
+           Skills  </Link>
+            </li>
+            </span>
+          
+            <li>
+            <Link className={Styles.LinkStyles}  to="/Projects" >
+            Projects </Link>
+            </li>
+            
+  </ul>
+        </aside >
+      </nav>
+     </header>
 
   </>
   )
@@ -10,4 +51,4 @@ const App = () => {
 
 
 
-export default App();
+export default Navbar;
