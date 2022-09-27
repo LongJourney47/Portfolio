bb087bb2741f6c98cba82075bac169b6c826e977
LongJourney47 <fernandezjw07@gmail.com>
2022-09-27 06:51:22 -0400

    feat: reactivity for the fixed links and images using hover effects.

--- src/Pages/Skills.js
+++ src/Pages/Skills.js
@@ -24,7 +24,7 @@ const Skills = () => {
       <div className={Styles.InfoCard}>
 
       <a href='https://developer.mozilla.org/en-US/docs/Web/javascript' target="_blank">
-      <div className={Styles.CardImage}>
+      <div className={Styles.CardImage} data-hover='Click me to learn more!'>
       
           <img width="100" height="100" src={myJSSvg}></img>
          
@@ -34,13 +34,15 @@ const Skills = () => {
       <p className={Styles.CardContent}> This section will simply be some placeholder text to assess if the spacing is ideal.
         Next part is to test the functionality of added code that should hide content when it reaches the sixth line within the 
         specified space provided.</p>
+      {/* <p className={Styles.CardContent}> As the first programming language I learned I have gained most of my expierence with coding using this.
+      Creating and expirmenting with a array of projects/functionalities that can be further explored on my project page. </p> */}
     </li>
 
     <li className={Styles.Technology}>
-    <h3>HTML</h3>
+    <h3>HTML5</h3>
       <div className={Styles.InfoCard}>
         <a href='https://developer.mozilla.org/en-US/docs/Web/HTML' target="_blank">
-        <div className={Styles.CardImage}>
+        <div className={Styles.CardImage} data-hover='Click me to learn more!'>
         <img width="100" height="100" src={myHTMLSvg}></img>
         </div>
         </a>
@@ -51,12 +53,12 @@ const Skills = () => {
     </li>
 
     <li className={Styles.Technology}>
-    <h3>CSS</h3>
-      <div className={Styles.InfoCard}>
+    <h3>CSS3</h3>
+      <div className={Styles.InfoCard} >
 
       <a href='https://developer.mozilla.org/en-US/docs/Web/CSS' target="_blank">
 
-        <div className={Styles.CardImage}>
+        <div className={Styles.CardImage} data-hover='Click me to learn more!'>
         <img width="100" height="100" src={myCSSSvg}></img>
         </div>
       </a>
@@ -71,7 +73,7 @@ const Skills = () => {
       <div className={Styles.InfoCard}>
         <a href='https://reactjs.org/' target="_blank">
 
-        <div className={Styles.CardImage}>
+        <div className={Styles.CardImage} data-hover='Click me to learn more!'>
         <img width="100" height="100" src={myReactSvg}></img>
         </div>
         </a>
@@ -80,13 +82,18 @@ const Skills = () => {
       <p className={Styles.CardContent}> This section will simply be some placeholder text to assess if the spacing is ideal.
         Next part is to test the functionality of added code that should hide content when it reaches the sixth line within the 
         specified space provided.</p>
+
+
+      {/* <p className={Styles.CardContent}> I have gained a proficent level of knowledge using react through my time at Code The Dream. Undergoing there spring
+      react course were I created a todo list using react and airtable. Branching from this I have created a number of projects in react, exploring different
+      approaches to create interesting applications.</p> */}
     </li>
 
     <li className={Styles.Technology}>
     <h3>Firebase</h3>
       <div className={Styles.InfoCard}>
       <a href='https://firebase.google.com/docs/' target="_blank">
-        <div className={Styles.CardImage}>
+        <div className={Styles.CardImage} data-hover='Click me to learn more!'>
          
         <img width="100" height="100" src={myFirebaseSvg}></img>
         </div>
@@ -98,10 +105,10 @@ const Skills = () => {
     </li>
 
     <li className={Styles.Technology}>
-    <h3>Python</h3>
+    <h3>Python3</h3>
       <div className={Styles.InfoCard}>
         <a href='https://www.python.org/about/' target="_blank">
-        <div className={Styles.CardImage}>
+        <div className={Styles.CardImage} data-hover='Click me to learn more!'>
         <img width="100" height="100" src={myPythonSvg}></img>
         </div>
          </a>
@@ -115,7 +122,7 @@ const Skills = () => {
     <h3>PostgreSQL</h3>
       <div className={Styles.InfoCard}>
         <a href='https://www.postgresql.org/about/' target="_blank">
-        <div className={Styles.CardImage}>
+        <div className={Styles.CardImage} data-hover='Click me to learn more!'>
         <img width="100" height="100" src={myPostgreSQLSvg}></img>
         </div>
          </a>
@@ -129,7 +136,7 @@ const Skills = () => {
    <h3>Git</h3>
       <div className={Styles.InfoCard}>
         <a href='https://git-scm.com/about' target="_blank">
-        <div className={Styles.CardImage}>
+        <div className={Styles.CardImage} data-hover='Click me to learn more!'>
         <img width="100" height="100" src={myGitSvg}></img>
         </div>
          </a>
@@ -139,15 +146,7 @@ const Skills = () => {
         specified space provided.</p>
     </li>
 
-    
-    {/* <li>Javascript</li>
-    <li>HTML</li>
-    <li>CSS</li>
-    <li>React</li>
-    <li>Firebase</li>
-    <li>Python</li>
-    <li>PostgreSQL</li>
-    <li>Git</li> */}
+ 
 
   </ul>
 </section>
