a61eee171061435ddc2178d707bcdb3d56ba272a
LongJourney47 <fernandezjw07@gmail.com>
2024-08-24 12:02:41 -0400

    feat: updated the about excerpt & packages

--- app/Pages/about/page.tsx
+++ app/Pages/about/page.tsx
@@ -11,24 +11,26 @@ const About: React.FC = () => {
     <section className="min-h-screen bg-cBrown text-white flex">
       <div className="text-white-500 center mb-5 mr-5 max-w-[40%] text-[1.5em] font-roboto mt-[10%] ml-[40px]">
         <p className="mb-5">
-          The name is William albeit people normally call me Will. Back in 2021
-          I was in the middle of wanting to make a change both in career
-          prospects and goals. Which eventually led me to reignite an old
-          passion for programming that continues into today. From solo studies,
-          to bootcamp, to apprenticeship the eagerness to build new projects
-          continues.
-        </p>
-        <p className="mb-5">
-          The primary focus now is to continue improving my Frontend Dev skills
-          as there is always something new to learn and ways I can improve.
-          While I am not doing that I take time to understand more of the
-          backend. That can enable me to have a more rounded perspective and
-          experience with development.
-        </p>
-
-        <p>
-          When I am not coding I like to study history, exercise, hike, explore,
-          read, and try my hand in art of various forms.
+          Before the begining to one of my longest & rewarding adventures back
+          in 2021 I just finished college with a major in Psychology but wanted
+          to make a change. I heard about coding for a while and davled a bit
+          when I had complications with my computer or an app. Once I began the
+          more I found myself burning to expand my knowledge & thought it would
+          be a good place to explore my creativity. Fast forward to today, & I
+          have two certifications in Front-end Web Development by Code The
+          Dream(also known as CTD) and soon to be a certification in python.
+          Moreover, currently have the pleasure to work as an apprentice for SAS
+          data and AI solutions. <br className="mb-5" />
+          My main focus for now centers on expanding my knowledge in UI/UX
+          design & some backend work with Golang as part of the needs for SAS.
+          Which I find to be great as I prefer that middle ground where design &
+          engineering meet.
+          <br className="mb-5" /> In my off time I try to continue this theme
+          with even personal projects with the intent expanding my knowledge so
+          that it can be transfered to other aspects of my life, namely my
+          career. Albeit, also to still maintain the creative outlet & to just
+          enjoy the adventure. When im not tied to my computer, I normally to go
+          out for hikes, read, exercise, & explore art.
         </p>
       </div>
 
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,21 +9,21 @@ const config: Config = {
   theme: {
     extend: {
       colors: {
-        cBrown: "#65451F"
+        cBrown: "#65451F",
       },
       fontFamily: {
-        roboto: ['Roboto', 'sans'],
-        bebas: ['Bebas Neue', 'sans'],
+        roboto: ["Roboto", "sans"],
+        bebas: ["Bebas Neue", "sans"],
       },
       keyframes: {
         cardFlipTopFwd: {
-          '0%': {
-            transform: 'translateY(0) translateZ(0) rotateX(0)',
-            transformOrigin: '50% 0%',
+          "0%": {
+            transform: "translateY(0) translateZ(0) rotateX(0)",
+            transformOrigin: "50% 0%",
           },
-          '100%': {
-            transform: 'translateX(-100%) translateZ(-160px) rotateY(-180deg)',
-            transformOrigin: '50% 100%',
+          "100%": {
+            transform: "translateX(-100%) translateZ(-160px) rotateY(-180deg)",
+            transformOrigin: "50% 100%",
           },
         },
         trackingInContact: {
@@ -43,14 +43,14 @@ const config: Config = {
           "0%": {
             letterSpacing: "-0.6em",
             transform: "translateZ(-700px) translateY(-500px)",
-            opacity: 0,
+            // opacity: 0,
           },
           "40%": {
-            opacity: 0.6,
+            // opacity: 0.6,
           },
           "100%": {
             transform: "translateZ(0) translateY(0)",
-            opacity: 1,
+            // opacity: 1,
           },
         },
       },
@@ -59,16 +59,15 @@ const config: Config = {
           "trackingExpandFwdTop 1.5s cubic-bezier(0.215, 0.610, 0.355, 1.000) both",
         trackingInContact:
           "trackingInContact 1.5s cubic-bezier(0.215, 0.610, 0.355, 1.000) both",
-        pulse: 'pulse 1s cubic-bezier(0.4, 0, 0.6, 1) infinite',
-        cardFlipTopFwd: 'cardFlipTopFwd 0.5s cubic-bezier(0.455, 0.030, 0.515, 0.955) both',
+        pulse: "pulse 1s cubic-bezier(0.4, 0, 0.6, 1) infinite",
+        cardFlipTopFwd:
+          "cardFlipTopFwd 0.5s cubic-bezier(0.455, 0.030, 0.515, 0.955) both",
       },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
-
-
     },
   },
   plugins: [],
